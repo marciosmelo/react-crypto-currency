cd342af4133518ecf58f43b3e73414aeaa6158cb
marciomelo@gmail.com <marciomelo@gmail.com>
2018-01-21 12:17:04 -0300

    Creating with Axios the list of BTC, ETH and IOTA currencies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+var NumberFormat = require('react-number-format');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+    .then(res => {
+      const cryptos = res.data;
+      console.log(cryptos);
+      this.setState({cryptos: cryptos});
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+       <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">CryptoCurrencies</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Bitcoin, Etherium and IOTA values
         </p>
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+            <span className="right"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></span>
+          </div>
+          ))}
       </div>
-    );
+    )
   }
 }
 
